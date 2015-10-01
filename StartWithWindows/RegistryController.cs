ec1e49c33bb8a62260aa16365bc3833b88e2e2fe
unknown <eduardo.cecon@gmail.com>
2015-10-01 15:54:05 -0300

    INITIAL COMMIT

---
+++ StartWithWindows/RegistryController.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace StartWithWindows
+{
+    public static class RegistryController
+    {
+        const string RunKey = @"SOFTWARE\Microsoft\Windows\CurrentVersion\Run";
+        /// <summary>
+        /// Adicionar/Remover entradas do windows startup
+        /// </summary>
+        /// <param name="appName">Nome da aplicação.</param>
+        /// <param name="enable">habilitar/desabilitar a entrada</param>
+        public static void SetStartup(string appName, bool enable)
+        {
+            if (enable)
+            {
+                if (Exists(appName)) return;
+                SetRegistry(appName);
+            }
+            else
+            {
+                // remove startup
+                RemoveRegistry(appName);
+            }
+        }
+
+        public static void SetRegistry(string appName, string key = RunKey)
+        {
+            using (var startupKey = Registry.LocalMachine.OpenSubKey(key, true))
+            {
+                if (startupKey == null) return;
+                startupKey.SetValue(appName, Application.ExecutablePath);
+
+            }
+        }
+        public static void RemoveRegistry(string appName, string key = RunKey)
+        {
+            using (var startupKey = Registry.LocalMachine.OpenSubKey(key, true))
+            {
+                if (startupKey == null) return;
+                startupKey.DeleteValue(appName, false);
+            }
+        }
+
+        public static bool Exists(string appName, string key = RunKey)
+        {
+            using (var startupKey = Registry.LocalMachine.OpenSubKey(key))
+            {
+                if (startupKey == null) return false;
+                return (startupKey.GetValue(appName) != null);
+            }
+        }
+    }
+}
---
+++ StartWithWindows/StartManager.cs
@@ -0,0 +1,27 @@
+﻿namespace StartWithWindows
+{
+    public class StartManager
+    {        
+        public static StartManager Current { get; set; }
+        public string AppName { get; set; }
+
+        public StartManager(string appName)
+        {
+            AppName = appName;
+        }
+
+        public bool Active
+        {
+            get { return RegistryController.Exists(AppName); }
+        }
+
+        public bool ToggleRegistry()
+        {
+            if (Active)
+                RegistryController.SetRegistry(AppName);            
+            else                        
+                RegistryController.RemoveRegistry(AppName);
+            return Active;
+        }
+    }
+}
